739cef415cd97ac76c85a82346861bfe8b54309e
dhayananthdevaraj <dhayan.d3@gmail.com>
2022-11-13 08:31:51 +0530

    simple calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [calc,setCalc] = useState("");
+const op = ['/','*','-','+'];
+const uc = (value) => {
+if(op.includes(value)&&calc ==="" ||
+op.includes(value)&&op.includes(calc.slice(-1
+)))
+return;
+setCalc(calc+value);
 }
-
+const calcul = () => {
+setCalc(eval(calc).toString());
+}
+const Del = () => {
+if(calc==="")
+return;
+const np = calc.slice(0,-1);
+setCalc(np);
+}
+return (
+<div className="App">
+<div className='cal'>
+<div className='display'>
+{calc||0}
+</div>
+<div className='opert'>
+<button onClick={() =>uc('/')}>/</button>
+<button onClick={() =>uc('*')}>*</button>
+<button onClick={() =>uc('-')}>-</button>
+<button onClick={() =>uc('+')}>+</button>
+<button onClick={Del}>Del</button>
+</div>
+<div className='digits'>
+<button onClick={() =>uc('1')}>1</button>
+<button onClick={() =>uc('2')}>2</button>
+<button onClick={() =>uc('3')}>3</button>
+<button onClick={() =>uc('4')}>4</button>
+<button onClick={() =>uc('5')}>5</button>
+<button onClick={() =>uc('6')}>6</button>
+<button onClick={() =>uc('7')}>7</button>
+<button onClick={() =>uc('8')}>8</button>
+<button onClick={() =>uc('9')}>9</button>
+<button onClick={() =>uc('0')}>0</button>
+<button onClick={() =>calcul()}>=</button>
+</div>
+</div>
+</div>
+);
+}
 export default App;
